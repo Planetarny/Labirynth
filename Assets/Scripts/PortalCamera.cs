29798f78622896fd07f0ffc951c9ccdfe34c391d
Planetarny <filipgrole12052012@gmail.com>
2025-05-25 16:15:17 +0200

    added portal cam rotation mechanism

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -16,7 +16,12 @@ public class PortalCamera : MonoBehaviour
     void PortalCameraController()
     {
 
-
+        Vector3 playerOffset = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffset;
+        float angularDiffrence= Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        Quaternion portalRotationalDiffrence = Quaternion.AngleAxis(angularDiffrence, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDiffrence * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection,Vector3.up);
 
     }
 }
