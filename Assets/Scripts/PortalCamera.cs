2ec917268bff65d0c35c40e6fcdbf1acb7cdd89a
Planetarny <filipgrole12052012@gmail.com>
2025-05-25 18:49:18 +0200

    fixed even more bugs

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -21,7 +21,7 @@ public class PortalCamera : MonoBehaviour
         float angularDiffrence= Quaternion.Angle(portal.rotation, otherPortal.rotation);
         Quaternion portalRotationalDiffrence = Quaternion.AngleAxis(angularDiffrence, Vector3.up);
         Vector3 newCameraDirection = portalRotationalDiffrence * playerCamera.forward;
-        newCameraDirection = new Vector3(newCameraDirection.x, newCameraDirection.y, newCameraDirection.z * -1);
+        newCameraDirection = new Vector3(newCameraDirection.x * -1, newCameraDirection.y, newCameraDirection.z * -1);
         transform.rotation = Quaternion.LookRotation(newCameraDirection,Vector3.up);
 
     }
