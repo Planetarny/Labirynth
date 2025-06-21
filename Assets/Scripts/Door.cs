648afd5219dbc42bca46b3326359f42a6a567825
Planetarny <filipgrole12052012@gmail.com>
2025-06-21 10:48:02 +0200

    Added doors function

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public bool open;
+    public float speed = 1.0f;
+
+    public void Open()
+    {
+
+        open =true ;
+
+
+    }
+    
+}
